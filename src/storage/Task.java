94b0eaacbfd80f629d7c876fb060922a5ee04e49
pznp111 <a0108355@nus.edu.sg>
2015-09-22 16:35:46 +0800

    storage

--- src/storage/Task.java
+++ src/storage/Task.java
@@ -1,5 +1,7 @@
 package storage;
 
+//data structure to store tasks
+
 public class Task {
 String name;
 
@@ -9,11 +11,14 @@ this.name = name;
 }
 
 //public methods
+
+//return the name of the task
 public String getName(){
 	
 	return name;
 }
 
+//change the name of the task
 public Boolean replaceName(String newName){
 	this.name = newName;
 	return true;
