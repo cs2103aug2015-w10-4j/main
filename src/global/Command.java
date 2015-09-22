ebfdd3eb9bd4e87a1f4688a7f26c17241c19d0bc
m133225 <m133225@hotmail.com>
2015-09-22 20:05:43 +0800

    Integrated the parts to work together, and other changes.

--- src/global/Command.java
+++ src/global/Command.java
@@ -1,17 +1,38 @@
 package global;
 
+import java.util.ArrayList;
+
 public class Command {
 
 	private Type commandType;
+	private ArrayList<String> argumentList;
 	private Task task;
 	
 	public enum Type {
 		add, edit, delete, display, exit;
 	}
 	
+	public Command(Type commandType){ 
+		this.commandType = commandType;
+	}
 	public Command(Type commandType, Task task){ 
 		this.commandType = commandType;
 		this.task = task;
+	}
+	
+	public Command(Type commandType, String[] args){ 
+		this.commandType = commandType;
+		for(int i = 0; i < args.length; i++){
+			argumentList.add(args[i]);
+		}
+	}
+	
+	public Command(Type commandType, String[] args, Task task){ 
+		this.commandType = commandType;
+		this.task = task;
+		for(int i = 0; i < args.length; i++){
+			argumentList.add(args[i]);
+		}
 	}
 	
 	// --------------- getter methods --------------------
@@ -19,7 +40,7 @@ public class Command {
 		return commandType;
 	}
 	
-	public getTask() {
+	public Task getTask() {
 		return task;
 	}
 	
--- src/storage/storage.java
+++ src/storage/storage.java
@@ -7,67 +7,66 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import global.Task;
 
-public class storage {
+public class Storage {
 	public static String filePath="save.txt";
-	
 	/**
 	 * save task in the file
 	 * @param ArrayList<Task>task    ArrayList that stores the tasks
 	 * @return   true if file is saved
 	 * @throws IOException 
 	 */
-public static boolean writeitem(ArrayList<Task>task) throws IOException{
-	String content = "";
-	for(int i = 0; i <task.size(); i ++){
-		content += task.get(i).getName() + "\r\n";
-	}
+	public boolean writeitem(ArrayList<Task> task) throws IOException{
+		String content = "";
+		for(int i = 0; i <task.size(); i ++){
+			content += task.get(i).getName() + "\r\n";
+		}
 	
-	File file = new File(filePath);
-	FileWriter fw = new FileWriter(file.getAbsoluteFile());
-	BufferedWriter bw = new BufferedWriter(fw);
-	bw.write(content);
+		File file = new File(filePath);
+		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(content);
 	//bw.newLine();
- 	bw.close();
-	return true;
-}
+		bw.close();
+		return true;
+	}
 
-/**
- *save result to a path
- *
- * @param path path is a String contain the path of file to save
- * @return   true if location changes
- * @throws IOException
- */
-public static boolean saveFileToPath(String path) throws IOException{
-	File file = new File(path);
-	if (!file.exists()){
-		file.createNewFile();
+	/**
+	 *save result to a path
+	 *
+	 * @param path path is a String contain the path of file to save
+	 * @return   true if location changes
+	 * @throws IOException
+	 */
+	public boolean saveFileToPath(String path) throws IOException{
+		File file = new File(path);
+		if (!file.exists()){
+			file.createNewFile();
+			filePath = path;
+			return false;
+		}
 		filePath = path;
-		return false;
+		return true;
 	}
-	filePath = path;
-	return true;
-}
 
-/**
- * the function read the saved file and return ArrayList that holds tasks
- * @return     ArrayList that contain tasks that has created
- * @throws FileNotFoundException if there is no file in the filePath
- */
-public static ArrayList<Task> getItemList() throws FileNotFoundException{
-	File file = new File(filePath);
-	Scanner sc = new Scanner(file);
-	ArrayList<Task>tasklist = new ArrayList<Task>();
+	/**
+	 * the function read the saved file and return ArrayList that holds tasks
+	 * @return     ArrayList that contain tasks that has created
+	 * @throws FileNotFoundException if there is no file in the filePath
+	 */
+	public ArrayList<Task> getItemList() throws FileNotFoundException{
+		File file = new File(filePath);
+		Scanner sc = new Scanner(file);
+		ArrayList<Task>tasklist = new ArrayList<Task>();
 	
-	while (sc.hasNext()){
-		String taskName=sc.next();
-		Task tempTask = new Task(taskName);
-		tasklist.add(tempTask);
+		while (sc.hasNext()){
+			String taskName=sc.next();
+			Task tempTask = new Task(taskName);
+			tasklist.add(tempTask);
+		}
+		sc.close();
+		return tasklist;
 	}
-	
-
-return tasklist;
-}
 
 }
