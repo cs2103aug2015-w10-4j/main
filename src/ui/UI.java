141efaa6c8b2c99a87a86114c4ed2139bd52738b
m133225 <m133225@hotmail.com>
2015-09-23 14:31:46 +0800

    Added focus to user input and clearing of input text when user presses esc

--- src/ui/UI.java
+++ src/ui/UI.java
@@ -3,12 +3,19 @@ package ui;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.beans.PropertyChangeListener;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 
 public class UI {
 	/*
@@ -48,6 +55,26 @@ public class UI {
 			}
 		});
 		
+		/*
+		 * Clears userInputField when "esc" is pressed
+		 */
+		@SuppressWarnings("serial")
+		Action clearText = new AbstractAction() {
+		    public void actionPerformed(ActionEvent e) {
+		        userInputField.setText("");
+		    }
+		};
+		userInputField.getInputMap().put(KeyStroke.getKeyStroke("ESCAPE"), clearText);
+		
+		/*
+		 * Focus given to userInputField when window is activated
+		 */
+		frame.addWindowListener(new WindowAdapter(){
+	        public void windowActivated(WindowEvent e) {
+	        	userInputField.grabFocus();
+	        }
+		});
+		
 		frame.add(mainPanel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
@@ -63,6 +90,7 @@ public class UI {
 	public String promptUser(String prompt) throws InterruptedException {
 		promptLabel.setText(prompt);
 		userInputField.setEditable(true);
+		userInputField.grabFocus();
 		synchronized (userInputField) {
 			while (userInputField.getText().isEmpty()) {
 				userInputField.wait();
