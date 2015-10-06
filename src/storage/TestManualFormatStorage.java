9db2c0d05f0cf424e9c5f14fc4c041b2e6a91b58
pznp111 <a0108355@nus.edu.sg>
2015-10-06 16:14:46 +0800

    remove magic number

--- src/storage/TestManualFormatStorage.java
+++ src/storage/TestManualFormatStorage.java
@@ -15,13 +15,19 @@ import logic.Logic;
 public class TestManualFormatStorage {
 Logic logicObject;
 ArrayList<Task> result ;
+String TEST_ITEMS = "item1 item2 item3 ";
 
 @Before
 public void setup(){
 	logicObject = new Logic();
 	result = new ArrayList<Task>();
-	Task newTask = new Task("item 1");
-	result.add(newTask);
+	String[]strArr = TEST_ITEMS.split(" ");
+	//Task newTask = new Task("item 1");
+	for(int i = 0; i < strArr.length; i++){
+		Task newTask = new Task(strArr[i]);
+		result.add(newTask);
+	}
+
 	
 	//File saveFile = new File("save.txt");
 	//saveFile.delete();
@@ -45,12 +51,10 @@ public void TestGetItemList() throws IOException{
 	ArrayList<Task> message = logicObject.storageObject.getItemList();
 	String result = "";
 	for(int i =0; i < message.size(); i++){
-		result += message.get(i).getName();
+		result += message.get(i).getName() + " ";
 	}
-	
-	
-		
-	assertEquals("item 1", result);
+			
+	assertEquals(TEST_ITEMS, result);
 }
 
 
