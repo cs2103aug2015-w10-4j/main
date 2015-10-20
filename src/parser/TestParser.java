917365dcf91b61664f3a92f4d850f54146a0fd66
pznp111 <a0108355@u.nus.edu>
2015-10-20 14:34:26 +0800

    init parser test

---
+++ src/parser/TestParser.java
@@ -0,0 +1,38 @@
+package parser;
+
+import static org.junit.Assert.assertEquals;
+import global.Command;
+import global.Task;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import storage.JsonFormatStorage;
+
+public class TestParser {
+
+	Parser parserObj;
+
+	@Before
+	public void setup(){
+			parserObj = Parser.getInstance();
+	}
+	
+	
+	@Test
+	public void testWriteItemList() {
+		Command message;
+		try {
+			message = parserObj.parseCommand("ADD");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			assertEquals("Error: Task name is empty", e.getMessage());
+		}
+	
+	}
+}
