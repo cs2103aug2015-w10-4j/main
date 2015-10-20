5dde35e578803a4f2141df68e4dc5689c571670f
pznp111 <a0108355@u.nus.edu>
2015-10-20 15:25:06 +0800

    some parser test cases added, need a lot more to complete

--- src/parser/TestParser.java
+++ src/parser/TestParser.java
@@ -24,15 +24,59 @@ public class TestParser {
 	
 	
 	@Test
-	public void testWriteItemList() {
+	public void testParserAddEmptyName() {
 		Command message;
 		try {
-			message = parserObj.parseCommand("ADD");
+			message = parserObj.parseCommand("add");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			assertEquals("Error: Task name is empty", e.getMessage());
 		}
 	
+	}
+	
+	@Test
+	public void testParserDeleteEmptyName() {
+		Command message;
+		try {
+			message = parserObj.parseCommand("delete");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			assertEquals("Error: Task name is empty", e.getMessage());
+		}
+	
+	}
+	
+	@Test
+	public void testParserEditEmptyName() {
+		Command message;
+		try {
+			message = parserObj.parseCommand("edit");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			assertEquals("Error: Task name is empty", e.getMessage());
+		}
+	
+	}
+	
+	@Test
+	public void testParserADD() throws Exception {
+		Command message;
+		Task task = new Task("task");
+		Command cmd = new Command(Command.Type.ADD,task);
+		message = parserObj.parseCommand("add task");
+		assertEquals(true,cmd.compareTo(message) );
+	}
+	
+	@Test
+	public void testParserDelete() throws Exception {
+		Command message;
+		Task task = new Task();
+		Command cmd = new Command(Command.Type.DELETE,task);
+		message = parserObj.parseCommand("delete 1");
+		assertEquals("DELETE",message.getCommandType().toString());
 	}
 }
