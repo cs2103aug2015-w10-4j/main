fa692a48ca36bc025e3f932cae951ffe67864f6e
raisfathin <rais.fathin38@gmail.com>
2015-10-25 03:08:03 +0800

    Set center alignment to table headers

--- src/ui/TaskTable.java
+++ src/ui/TaskTable.java
@@ -2,8 +2,12 @@ package ui;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
 
+import javax.swing.JLabel;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.JTableHeader;
 import javax.swing.table.TableCellRenderer;
 
 import ui.tasktable.TaskTableModel;
@@ -15,12 +19,27 @@ public class TaskTable extends JTable {
 	private static final Color ALTERNATE_COLOR = Color.LIGHT_GRAY;
 	private static final Color DONE_COLOR = Color.GREEN;
 	
+	private static final String HEADER_FONT_NAME = "SansSerif";
+	private static final int HEADER_FONT_STYLE = Font.BOLD;
+	private static final int HEADER_FONT_SIZE = 12;
+	
 	private TaskTableModel model = null;
 	
 	public TaskTable(TaskTableModel dm) {
 		super(dm);
 		
 		this.model = dm;
+		
+		prepareTable();
+	}
+	
+	private void prepareTable() {
+		JTableHeader tableHeader = getTableHeader();
+		tableHeader.setFont(new Font(HEADER_FONT_NAME, HEADER_FONT_STYLE, HEADER_FONT_SIZE));
+		
+		TableCellRenderer headerRenderer = tableHeader.getDefaultRenderer();
+		JLabel headerLabel = (JLabel) headerRenderer;
+		headerLabel.setHorizontalAlignment(SwingConstants.CENTER);
 	}
 	
 	public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
