0faa6176d70fb65ec79d7b684a1b0859254fe532
raisfathin <rais.fathin38@gmail.com>
2015-11-08 22:27:57 +0700

    Remove unnecessary include

--- src/ui/UI.java
+++ src/ui/UI.java
@@ -13,7 +13,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
