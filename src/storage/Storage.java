c04e583f32276712d527ad3772fffd5819b70bf8
m133225 <m133225@hotmail.com>
2015-11-09 09:47:35 +0800

    Replaced reading of help file with a static string as we are unable to submit a help file

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -11,6 +11,35 @@ import java.util.ArrayList;
 public interface Storage {
 	static final String LINE_SEPARATOR = System.getProperty("line.separator");
 	static final String HELP_PATH = "help.txt";
+	static final String HELP_MESSAGE =
+"Commands  	Description                                         Example usage\n"+
+"add        Create new task entry with optional fields:         add task123\n"+
+"           - Deadline: 'by'/'from...to'/'start...end'          add task123 by tomorrow 6PM\n"+
+"           - Location: 'at'/'loc'                              add task123 at playground\n"+
+"           - Periodic: 'every n days/weeks/years for n times'  add task123 by next thursday every 3 weeks for 10 times\n"+
+"\n"+
+"display    Display the default view of Tasky                   display\n"+
+"\n"+
+"edit       Edit task entry at specified index                  edit 1 task456 from 12 sep to 15 sep\n"+
+"                                                               edit 1 at town\n"+
+"\n"+
+"delete     Delete task entry at specified index                delete 1\n"+
+"\n"+
+"undo       Undo previous add, edit or delete                   undo\n"+
+"\n"+
+"redo       Redo previous add, edit or delete                   redo\n"+
+"\n"+
+"search     Search for a task with the specified keyword        search task456\n"+
+"           or field+value                                      search loc home\n"+
+"                                                               search by 18 sep\n"+
+"\n"+
+"mark       Marks a task at specified index as done             mark 1\n"+
+"\n"+
+"unmark     Unmarks a task at specified index as undone         unmark 1\n"+ 
+"\n"+
+"saveto     Specify the path of the savefile                    saveto new_file.txt\n"+
+"\n"+
+"exit       Exits the program                                   exit\n";
 	static final String ERROR_HELP = "Unable to retrieve help file!";
 
 	/**
@@ -39,19 +68,7 @@ public interface Storage {
 	
 	//@@author A0124093M
 	public default String getHelpMessage() {
-		BufferedReader br;
-		String mainStr = "";
-		try {
-			String currentLine;
-			br = new BufferedReader(new FileReader(HELP_PATH));
-			while ((currentLine = br.readLine()) != null) {
-				mainStr += currentLine + LINE_SEPARATOR;
-			}
-			br.close();
-		} catch (IOException e) {
-			return ERROR_HELP;
-		}
-		return mainStr;
+		return HELP_MESSAGE;
 	}
 	
 }
